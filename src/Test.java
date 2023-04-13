38033beaa1c6d4004257328dc8a5bb97e3917ef4
ayuen25 <130687914+ayuen25@users.noreply.github.com>
2023-04-13 12:36:48 -0400

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
         System.out.println("hello");
         System.out.println("whats up");
         System.out.println("Maximo is here");
+        System.out.println("how tf do i use this - AY");
     }
 
 }

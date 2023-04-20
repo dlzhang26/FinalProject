64a51e82b201d293948bc1bdb771cefdfff57c73
evandewater26 <130618127+evandewater26@users.noreply.github.com>
2023-04-20 10:33:27 -0400

    added startscreen class, unfinished atm

---
+++ src/StartScreen.java
@@ -0,0 +1,41 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.*;
+
+public class StartScreen implements KeyListener {
+//fields
+Boolean gameStart;
+
+    public StartScreen(){//default constructor
+        gameStart=false;//when leaderboard is first created, gameStart is false. 
+    }
+/////////////////////////////////////////////////////
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char c = e.getKeyChar();//when key is pressed it gets defined as char c, can use that to then do some things
+    }
+   
+    public void keyReleased(KeyEvent e) {
+       
+    }
+   
+    public void keyTyped(KeyEvent e) {
+       
+    }
+
+/////////////////////////////////////////////////////Methods from keyListener interface
+
+    public void draw(Graphics g,World w) {//this is where the 
+        Color c = g.getColor();
+
+       // g.setColor(color);
+        
+         while(gameStart==false){//while the game has not started... do some drawings
+
+        }
+       
+        g.setColor(c);
+
+    }
+
+}

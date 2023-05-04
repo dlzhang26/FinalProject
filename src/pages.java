22323e1bcb78c7a92628a8104e06fec9978317bc
evandewater26 <130618127+evandewater26@users.noreply.github.com>
2023-05-04 12:11:16 -0400

    pageswork

--- src/pages.java
+++ src/pages.java
@@ -4,8 +4,21 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 public class pages {
 
+    boolean soundon = true;
+
     public void chooseUserProfile(Graphics g){
         
+    }
+
+    public void soundOption(Graphics g){//making a sound toggle switch for music
+        if(soundon==true){
+            g.setColor(Color.white);
+        g.fillRect(280, 500, 80, 50);
+        g.setColor(Color.black);
+        g.drawString("Sound On", 300, 520);
+        }
+        g.fillRect(360, 500, 80, 50);
+
     }
     
 
